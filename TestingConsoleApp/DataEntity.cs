ec18df8324b916e5450086c6f94bf3ceab0db386
Htet Min Lu <htetminlu@gmail.com>
2026-07-04 19:51:28 +0630

    Add project files.

---
+++ TestingConsoleApp/DataEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingConsoleApp
+{
+    public class DataEntity
+    {
+        public class Student
+        {
+            public int StudentId { get; set; }
+            public string StudentName { get; set; }
+            public string StudentNo { get; set; }
+            public string FatherName { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Address { get; set; }
+            public DateTime BirthDate { get; set; }
+
+
+        }
+    }
+}
---
+++ TestingConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using TestingConsoleApp;
+
+AdoNetService adoNetService = new AdoNetService();
+DapperService dapperService = new DapperService();
+
+
+//adoNetService.Create();
+adoNetService.GetData();
+//adoNetService.Update();
+//adoNetService.Delete();
+
+
+//dapperService.Create();
+//dapperService.GetData();
+//dapperService.Update();
+//dapperService.Delete();
